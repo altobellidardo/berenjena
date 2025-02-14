b15c624e64d0ffd7ac9bb8ee9ee2be9e6e449383
Dardo Altobelli <altobellidardo@gmail.com>
2025-02-14 17:46:00 -0300

    feat: improve rooms page logic

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,39 +1,50 @@
-import AdminPage from "@/components/adminPage"
-import GuestPage from "@/components/guestPage"
-import { DbGetPlayer } from "@/libs/supabase/tables/players"
-import { isAdmin } from "@/utils/isAdmin"
-import { getMatchData } from "@/utils/matchData"
-import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
+import AdminPage from "@/components/adminPage";
+import GuestPage from "@/components/guestPage";
+import NoName from "@/components/noName";
+import { DbCreatePlayer, DbGetPlayer } from "@/libs/supabase/tables/players";
+import { isAdmin } from "@/utils/isAdmin";
+import { getMatchData } from "@/utils/matchData";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-interface RoomPageProps {
-  searchParams: Promise<{ id: string, name: string } | undefined>
+interface PageProps {
+  searchParams: Promise<{ id?: string; name?: string } | undefined>
 }
 
-async function RoomPage ({ searchParams }: RoomPageProps) {
+async function Page ({ searchParams }: PageProps) {
   const sParams = await searchParams
-  const id = sParams?.id
-  if (!id) redirect('/')
+  const roomId = sParams?.id
+  if (!roomId) redirect('/')
 
   const cookieStore = await cookies()
   const token = cookieStore.get('session')?.value
-  const res = await isAdmin(token, id)
+  const adminData = await isAdmin(token, roomId)
 
-  if (!res) {
-    const name = sParams?.name
-    if (!name) redirect('/')
+  // Es admin
+  if (adminData) {
+    const { adminId } = adminData
+    const players = await getMatchData(roomId)
 
-    const { data: newPlayer, error: playerError } = await DbGetPlayer(id, name)
-    if (playerError) redirect('/')
+    // refrescar token
+    // const adminName = players.find((player) => player.id === adminId)?.name
+    // const newToken = signToken({ id: adminId, name: adminName }, '1h')
+    // cookieStore.set('session', newToken, { httpOnly: true, secure: true, path: '/' })
 
-    return <GuestPage player={newPlayer} />
+    return <AdminPage players={players} adminId={adminId} roomId={roomId} />
   }
 
-  const { adminId } = res
+  // No es admin
+  const name = sParams?.name?.trim()
+  if (!name) return <NoName roomId={roomId} />
 
-  const players = await getMatchData(id)
+  // el jugador existe?
+  const { data: playerExists } = await DbGetPlayer(roomId, name)
+  if (playerExists) return <GuestPage player={playerExists} />
 
-  return <AdminPage players={players} adminId={adminId} roomId={id} />
+  // el jugador no existe
+  const { data: newPlayer } = await DbCreatePlayer(name, roomId)
+
+  return <GuestPage player={newPlayer} />
 }
 
-export default RoomPage
+export default Page
