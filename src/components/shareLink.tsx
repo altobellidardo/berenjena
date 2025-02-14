a0c836d54ebb7fdf99666544751b0dbfa4aa8859
Dardo Altobelli <altobellidardo@gmail.com>
2025-02-14 17:45:22 -0300

    feat: implement share link component

--- src/components/shareLink.tsx
+++ src/components/shareLink.tsx
@@ -2,18 +2,44 @@
 
 import Link from "next/link"
 import Button from "./ui/button"
+import { useEffect, useState } from "react"
+
+const TEXT = {
+  COPY: 'Copiar',
+  COPIED: '¡Copiado!'
+}
 
 interface ShareLinkProps {
   route: string
 }
 
 function ShareLink ({ route }: ShareLinkProps) {
-  cont 
+  const [link, setLink] = useState('')
+  const [buttonText, setButtonText] = useState(TEXT.COPY)
+
+  useEffect(() => {
+    const baseUrl = global.location.origin
+    setLink(`${baseUrl}${route}`)
+  }, [route])
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(link)
+    setButtonText(TEXT.COPIED)
+    setTimeout(() => {
+      setButtonText(TEXT.COPY)
+    }, 1000)
+  }
+
   return (
-    <div className="flex gap-2">
-      <p>Link de invitación</p>
-      <Link href={`/room?id=${roomId}&name=${adminId}`} className="text-brand-midnight font-bold">{`/room?id=${roomId}&name=${adminId}`}</Link>
-      <Button>Copiar</Button>
+    <div className="flex flex-col w-fit">
+      <p className="flex gap-2 items-center">Invitación
+        <Link href={link} className="text-brand-midnight font-bold text-sm hover:underline">
+          {link}
+        </Link>
+      </p>
+      <Button className="h-6" onClick={handleCopy} disabled={buttonText === TEXT.COPIED}>
+        {buttonText}
+      </Button>
     </div>
   )
 }
